602e81d16cd9d21bf5ee347de18e8244391f7c3d
Undrax01 <45149539+Undrax01@users.noreply.github.com>
2020-01-12 20:50:14 +0800

    graphic duus

--- src/app/statistic/statistic.component.ts
+++ src/app/statistic/statistic.component.ts
@@ -17,10 +17,9 @@ import { AuthService } from "src/services/auth.service";
 })
 export class StatisticComponent implements OnInit {
   arr: Array<any> = [];
-  doughnutChartLabels: Label[] = ["BMW", "Ford", "Tesla"];
-  doughnutChartData: MultiDataSet = [[55, 25, 20]];
-  doughnutChartType: ChartType = "doughnut";
-
+  arr2: Array<any> = [];
+  arr3: Array<any> = [];
+  // bar1
   barChartOptions: ChartOptions = {
     responsive: true
   };
@@ -41,9 +40,69 @@ export class StatisticComponent implements OnInit {
     { data: [0, 0, 0, 0, 0, 0], label: "" },
     { data: [0, 0, 0, 0, 0, 0], label: "" },
     { data: [0, 0, 0, 0, 0, 0], label: "" },
+    { data: [0, 0, 0, 0, 0, 0], label: "" },
+    { data: [0, 0, 0, 0, 0, 0], label: "" },
     { data: [0, 0, 0, 0, 0, 0], label: "" }
   ];
   barChartColors: Color[] = [
+    // {
+    //   borderColor: "white",
+    //   backgroundColor: [
+    //     "rgb(255, 202, 90)"
+    //   ]
+    // }
+  ];
+  // bar 2
+  barChartOptions2: ChartOptions = {
+    responsive: true
+  };
+  barChartLabels2: Label[] = [
+    "Талархал",
+    "Ирсэн",
+    "Харсан",
+    "Цуцлагдсан",
+    "Баталгаажаагүй",
+    "Баталгаажсан"
+  ];
+  barChartType2: ChartType = "bar";
+  barChartLegend2 = true;
+  barChartPlugins2 = [];
+
+  barChartData2: ChartDataSets[] = [
+    { data: [0, 0, 0, 0, 0, 0], label: "" },
+    { data: [0, 0, 0, 0, 0, 0], label: "" }
+  ];
+  barChartColors2: Color[] = [
+    // {
+    //   borderColor: "white",
+    //   backgroundColor: [
+    //     "rgb(255, 202, 90)"
+    //   ]
+    // }
+  ];
+
+  // bar 3
+  barChartOptions3: ChartOptions = {
+    responsive: true
+  };
+  barChartLabels3: Label[] = [
+    "Талархал",
+    "Ирсэн",
+    "Харсан",
+    "Цуцлагдсан",
+    "Баталгаажаагүй",
+    "Баталгаажсан"
+  ];
+  barChartType3: ChartType = "bar";
+  barChartLegend3 = true;
+  barChartPlugins3 = [];
+
+  barChartData3: ChartDataSets[] = [
+    { data: [0, 0, 0, 0, 0, 0], label: "" },
+    { data: [0, 0, 0, 0, 0, 0], label: "" },
+    { data: [0, 0, 0, 0, 0, 0], label: "" }
+  ];
+  barChartColors3: Color[] = [
     // {
     //   borderColor: "white",
     //   backgroundColor: [
@@ -99,8 +158,8 @@ export class StatisticComponent implements OnInit {
     });
     this.postService.getB1(this.auth.getUser.own_code).subscribe(res => {
       this.arr.push(Object.keys(res));
-      // console.log("------------->", this.arr[0][0]);
-      for (let i = 0; i < 6; i++) {
+      // console.log("------------->", res);
+      for (let i = 0; i < 7; i++) {
         this.barChartData[i].label = this.arr[0][i];
         this.barChartData[i].data[0] = res[this.arr[0][i]].thanks;
         this.barChartData[i].data[1] = res[this.arr[0][i]].sent;
@@ -108,6 +167,31 @@ export class StatisticComponent implements OnInit {
         this.barChartData[i].data[3] = res[this.arr[0][i]].reject;
         this.barChartData[i].data[4] = res[this.arr[0][i]].pending;
         this.barChartData[i].data[5] = res[this.arr[0][i]].done;
+      }
+    });
+
+    this.postService.getB2(this.auth.getUser.own_code).subscribe(res => {
+      this.arr2.push(Object.keys(res));
+      for (let i = 0; i < 2; i++) {
+        this.barChartData2[i].label = this.arr2[0][i];
+        this.barChartData2[i].data[0] = res[this.arr2[0][i]].thanks;
+        this.barChartData2[i].data[1] = res[this.arr2[0][i]].sent;
+        this.barChartData2[i].data[2] = res[this.arr2[0][i]].seen;
+        this.barChartData2[i].data[3] = res[this.arr2[0][i]].reject;
+        this.barChartData2[i].data[4] = res[this.arr2[0][i]].pending;
+        this.barChartData2[i].data[5] = res[this.arr2[0][i]].done;
+      }
+    });
+    this.postService.getA().subscribe(res => {
+      this.arr3.push(Object.keys(res));
+      for (let i = 0; i < 3; i++) {
+        this.barChartData3[i].label = this.arr3[0][i];
+        this.barChartData3[i].data[0] = res[this.arr3[0][i]].thanks;
+        this.barChartData3[i].data[1] = res[this.arr3[0][i]].sent;
+        this.barChartData3[i].data[2] = res[this.arr3[0][i]].seen;
+        this.barChartData3[i].data[3] = res[this.arr3[0][i]].reject;
+        this.barChartData3[i].data[4] = res[this.arr3[0][i]].pending;
+        this.barChartData3[i].data[5] = res[this.arr3[0][i]].done;
       }
     });
   }
